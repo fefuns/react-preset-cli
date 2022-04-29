5d517ce708dc2757477f537d7063b8f7418ca30c
fefuns <1321120469@qq.com>
2022-04-30 06:50:18 +0800

    feat: 组件通信-兄弟组件通信

---
+++ src/views/Connect/Brother/index.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+function Son1(props) {
+  return (
+    <>
+      <h2>兄弟1：显示number</h2>
+      <p>兄弟1接收的number: {props.number}</p>
+    </>
+  )
+}
+function Son2(props) {
+  return (
+    <>
+      <h2>兄弟2：需要去改变兄弟1的number</h2>
+      <input type="number" onChange={props.onChange}/>
+    </>
+  )
+}
+
+function Father() {
+  const [number, setNumber] = useState(0);
+  function handleChange(e) {
+    setNumber(e.target.value)
+  }
+  return (
+    <>
+      <h1>如果是兄弟组件之间的传递，则父组件作为中间层来实现数据的互通，通过使用父组件传递</h1>
+      <p>父组件number: {number}</p>
+      <Son1 number={number} onChange={handleChange}/>
+      <Son2 number={number} onChange={handleChange}/>
+    </>
+  )
+}
+
+export default Father
