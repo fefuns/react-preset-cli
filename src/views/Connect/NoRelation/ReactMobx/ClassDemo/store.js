84116c42767480003f4b12cf248b79495a8e4a44
fefuns <1321120469@qq.com>
2022-05-08 17:25:20 +0800

    feat: 组件通信

---
+++ src/views/Connect/NoRelation/ReactMobx/ClassDemo/store.js
@@ -0,0 +1,13 @@
+// import { observable } from "mobx";
+
+// class store {
+//   @observable modalShow = false;
+//   async getList() {
+//     await http(`/api/xxx/`).then(res => {
+//       const list = res.data || []
+//       runInAction(() => {
+//         this.modalShow = list.length > 0 ? true : false
+//       })
+//     })
+//   }
+// }
